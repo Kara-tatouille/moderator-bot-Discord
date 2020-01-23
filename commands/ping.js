f353eb7b01927ee14210cf84656750ab82fbcfb9
Corenbla <corenbla@gmail.com>
2020-01-23 15:55:58 +0100

    Dummy text added to ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,9 @@ module.exports = {
     name: 'ping',
     description: 'Ping!',
     cooldown: 5,
+    usage: '',
+    args: 0,
+    aliases: ['pong'],
     execute(message, args) {
         message.channel.send('Pong.');
     },
