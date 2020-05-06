e75bdb18672dae23e4204a3699e73392b4da630f
corenbla <corenbla@gmail.com>
2020-05-06 19:49:44 +0200

    added catch statements to the sql queries

--- helper/handleXp.js
+++ helper/handleXp.js
@@ -2,8 +2,14 @@ module.exports = function handleXp(message, connection) {
     connection.query(`SELECT * FROM dofus.user WHERE discord_id = ${message.author.id}`, (err, row) => {
         if (row.length === 0) { // Create row for new user
             connection.query(`INSERT INTO dofus.user (discord_id, username, xp) VALUES ('${message.author.id}', ${connection.escape(message.author.username)}, '1')`)
+                .catch(reason => {
+                    console.log("SQL insert error : \n" + reason)
+                })
         } else { // Add 10 xp
             connection.query(`UPDATE dofus.user SET xp = xp + 1 WHERE discord_id = ${message.author.id}`)
+                .catch(reason => {
+                    console.log("SQL update error : \n" + reason)
+                })
         }
     });
 };
