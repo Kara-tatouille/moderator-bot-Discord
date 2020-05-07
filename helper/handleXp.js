5cb6b1b48276fb11fb0ff6cc756bc98bbf16a948
corenbla <corenbla@gmail.com>
2020-05-07 08:48:36 +0200

    using try...catch block to prevent query from failing

--- helper/handleXp.js
+++ helper/handleXp.js
@@ -1,9 +1,17 @@
 module.exports = function handleXp(message, connection) {
     connection.query(`SELECT * FROM dofus.user WHERE discord_id = ${message.author.id}`, (err, row) => {
         if (row.length === 0) { // Create row for new user
-            connection.query(`INSERT INTO dofus.user (discord_id, username, xp) VALUES ('${message.author.id}', ${connection.escape(message.author.username)}, '1')`)
+            try {
+                connection.query(`INSERT INTO dofus.user (discord_id, username, xp) VALUES ('${message.author.id}', ${connection.escape(message.author.username)}, '1')`)
+            } catch (e) {
+                console.log(e)
+            }
         } else { // Add 10 xp
-            connection.query(`UPDATE dofus.user SET xp = xp + 1 WHERE discord_id = ${message.author.id}`)
+            try {
+                connection.query(`UPDATE dofus.user SET xp = xp + 1 WHERE discord_id = ${message.author.id}`)
+            } catch (e) {
+                console.log(e)
+            }
         }
     });
 };
