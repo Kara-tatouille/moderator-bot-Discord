54391f194a0c56adf97ffd7c1371a0e3ab08cb8f
corenbla <corenbla@gmail.com>
2020-05-11 12:10:39 +0200

    showXp command

---
+++ commands/showXp.js
@@ -0,0 +1,26 @@
+const ordinalSuffix = require('../helper/ordinalSuffix');
+
+
+module.exports = {
+    name: 'xp',
+    description: 'Show your xp',
+    cooldown: 60,
+    usage: 'xp',
+    args: 0,
+    aliases: [],
+    execute(message, args, connection) {
+        connection.query(`SELECT xp FROM user WHERE discord_id = ${message.author.id}`, (firstErr, firstRow) => {
+            if (firstRow.length === 0) {
+                return;
+            }
+            
+            connection.query(`SELECT id FROM user WHERE xp >= ${firstRow[0].xp}`, (secondErr, secondRow) => {
+                if (secondRow.length === 0) {
+                    return;
+                }
+
+                message.channel.send(`Hello ${message.author.username}. You currently have ${firstRow[0].xp} xp. You are ${ordinalSuffix(secondRow.length)} in the ladder.`)
+            })
+        })
+    },
+};
