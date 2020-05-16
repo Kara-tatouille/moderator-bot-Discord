694315848e9ed66f3f0e089a2c760167d4e4467a
corenbla <corenbla@gmail.com>
2020-05-16 11:26:12 +0200

    won't send to the bot channels commands used in DM

--- helper/handleSuggestion.js
+++ helper/handleSuggestion.js
@@ -1,9 +1,10 @@
 const quote = require('./quote');
 const sendToBotChannel = require('./sendToBotChannel');
+const {prefix} = require('../config/config.json');
 
 
 module.exports = function handleSuggestion(message) {
-    if (message.author.bot) return;
+    if (message.author.bot || message.content.startsWith(prefix)) return;
 
     sendToBotChannel(message.client, quote(message))
 };
