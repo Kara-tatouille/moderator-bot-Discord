e815d6df5c2c76c5f4a6ab03c2004b368fe8f69e
Kara Richard-Vitton <corenbla@gmail.com>
2021-06-29 11:19:27 +0200

    get xp command and show xp

---
+++ commands/getXp.js
@@ -0,0 +1,40 @@
+const ordinalSuffix = require('../helper/ordinalSuffix');
+
+
+module.exports = {
+    name: 'getXp',
+    description: 'Show xp of target username',
+    cooldown: 30,
+    usage: 'xp',
+    args: 1,
+    aliases: ['getxp'],
+    offChamberOnly: true,
+    async execute(message, args, connection) {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.react('❌');
+
+        let target;
+        try {
+            target = (await message.guild.members.fetch({query: args[0], limit: 1})).first()
+        } catch (e) {
+            return message.channel.send('Utilisateur non trouvé / User not found')
+        }
+
+        console.log(args)
+        console.log(target)
+
+
+        connection.query(`SELECT xp FROM user WHERE discord_id = ${target.id}`, (firstErr, firstRow) => {
+            if (firstRow.length === 0) {
+                return;
+            }
+
+            connection.query(`SELECT id FROM user WHERE xp >= ${firstRow[0].xp}`, (secondErr, secondRow) => {
+                if (secondRow.length === 0) {
+                    return;
+                }
+
+                message.channel.send(`${message.author.username} currently has ${firstRow[0].xp} xp. they are ${ordinalSuffix(secondRow.length)} in the ladder.`)
+            })
+        })
+    },
+};
--- commands/showXp.js
+++ commands/showXp.js
@@ -4,14 +4,12 @@ const ordinalSuffix = require('../helper/ordinalSuffix');
 module.exports = {
     name: 'xp',
     description: 'Show your xp',
-    cooldown: 60,
+    cooldown: 604800, // 1 week
     usage: 'xp',
     args: 0,
     aliases: [],
     offChamberOnly: true,
     execute(message, args, connection) {
-        if (!message.member.hasPermission('ADMINISTRATOR')) return message.react('❌');
-
         connection.query(`SELECT xp FROM user WHERE discord_id = ${message.author.id}`, (firstErr, firstRow) => {
             if (firstRow.length === 0) {
                 return;
